2a8dff5a33978f49fbad4c8d3384d826add43fab
Aki <210830810+iiTzAki@users.noreply.github.com>
2025-12-24 16:13:52 +0500

    add: new function to play rounds with 2 variables
    Make this for the main logic to the game with if
    else statements and two variables for keeping
    track

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Initializing two varaibles to keep track of the scores
 
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 // Function for getting Computer's turn
 
@@ -21,5 +21,52 @@ function getComputerChoice() {
 // Function for User's input prompt
 
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper, Scissors: ")
+    const humanChoice = prompt("Rock, Paper, Scissors: ")
+    return humanChoice;
 }
+
+
+// Function for the playing the round 
+
+function playRound(humanChoice, computerChoice) { 
+    const humanString = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    console.log(`${humanString} ⚔️ ${computerChoice}`)
+
+    // Game Logic 
+
+    if (humanString == computerChoice) {
+        console.log("Tie!")
+
+    } else if (humanString == "Rock" && computerChoice == "Paper") {
+        console.log("You lost! Paper beats Rock");
+        computerScore = ++computerScore
+
+    } else if (computerChoice == "Rock" && humanString == "Paper") { 
+        console.log("You won! Rock beats Paper");
+        humanScore = ++humanScore
+
+    } else if (humanString == "Scissors" && computerChoice == "Rock") {
+        console.log("You lost! Rock destroys Scissors");
+        computerScore = ++computerScore
+
+    } else if (computerChoice == "Scissors" && humanString == "Rock") {
+        console.log("You won! Rock destroys Scissors");
+        humanScore = ++humanScore
+
+    } else if (humanString == "Scissors" && computerChoice == "Paper") {
+        console.log("You won! Scissors cuts up Paper")
+        humanScore = ++humanScore
+
+    } else if (computerChoice == "Scissors" && humanString == "Paper") {
+        console.log("You lost! Paper gets cut up by Scissors")
+        computerScore = ++computerScore
+
+    } else {
+        console.error(new Error("Use Rock, Paper or Scissors"))
+    }
+}
+
+humanSelection = getHumanChoice();
+computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
